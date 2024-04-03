711500b43e42f418503ee1907a1b0540b97ccb95
shane <106403792+shane7632@users.noreply.github.com>
2024-04-04 00:13:01 +0800

    Update main.c

--- hw3/main.c
+++ hw3/main.c
@@ -13,28 +13,32 @@ int main() {
     int n;
     printf("請問你要買幾組樂透彩:");
     scanf("%d", &n);
-    
-   
-    
-    lotto_record_t number[n]; 
-    
-    srand(1); 
-    
+
+    lotto_record_t number[n];
+
+    srand(1);
+
     for (int i = 0; i < n; i++) {
         FILE *file;
         char filename[20];
         sprintf(filename, "lotto%d.txt", i + 1);
         file = fopen(filename, "w");
-     fprintf(file, "lotto649");
-     fprintf(file, "=======+No.00%d+=======",i);
-      fprintf(file, "=======+No.00%d+=======",i);
-        
+        if (file == NULL) {
+            printf("無法打開文件 %s\n", filename);
+            return 1; 
+        }
+        fprintf(file, "lotto649\n");
+        fprintf(file, "=======+No.00%d+=======\n", i+1);
+        time_t t = time(NULL);
+        struct tm *tm_info = localtime(&t);
+        fprintf(file, "%s\n", asctime(tm_info));
+
         for (int j = 0; j < 5; j++) {
-            int isrepeat[70] = {0}; 
+            int isrepeat[70] = {0};
             fprintf(file, "[%d]: ", j + 1);
             for (int k = 0; k < 7; k++) {
                 number[i].lotto_set[j][k] = rand() % 69 + 1;
-                
+
                 while (isrepeat[number[i].lotto_set[j][k]] == 1) {
                     number[i].lotto_set[j][k] = rand() % 69 + 1;
                 }
@@ -43,9 +47,23 @@ int main() {
             }
             fprintf(file, "\n");
         }
-        
+        fprintf(file, "=======csie@CGU===== ");
         fclose(file);
     }
-    
+
+
+    FILE *bin_file = fopen("record.bin", "wb"); 
+    if (bin_file == NULL) {
+        printf("Unable to open file record.bin\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+       
+        fwrite(&number[i], sizeof(lotto_record_t), 1, bin_file);
+    }
+
+    fclose(bin_file);
+
     return 0;
 }
